89c5978067409f1217c0e155c112bcbce7ed7a5b
João Paulo Ozório da Silva <jpozorio@gmail.com>
2017-07-23 10:09:44 -0300

    Corrige get value dos pontos do jogador na mão

--- src/pages/about/about.ts
+++ src/pages/about/about.ts
@@ -1,7 +1,7 @@
 import {Component} from "@angular/core";
 import {NavController, NavParams} from "ionic-angular";
 import {Game, Hand, Match, Player} from "../../app/customer.interface";
-import {FormArray, FormBuilder, FormGroup} from "@angular/forms";
+import {FormBuilder, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'page-about',
@@ -23,21 +23,9 @@ export class GamePage {
   ngOnInit() {
     this.handForm = this._fb.group(
       {
-        pontos: ['0'],
-        estourou: [false],
         handForm: this._fb.array([])
       }
     );
-
-    // for(let player of this.game.playersAtGame){
-    //   this.addHand(player)
-    // }
-  }
-
-  addHand(player: Player) {
-    const control = <FormArray>this.handForm.controls['handForm'];
-    const addrCtrl = this.initHand(player);
-    control.push(addrCtrl);
   }
 
   estourou(player: Player) {
@@ -47,19 +35,11 @@ export class GamePage {
     this.currentMatch.handsOfMatch.push(hand)
   }
 
-  setPontos(player: Player, i: number) {
-    const control = <FormArray>this.handForm.controls['handForm'];
+  setPontos(player: Player, i: number, value) {
     let hand = new Hand();
     hand.player = player;
-    console.log(control);
-    // hand.score = control.controls[i].get('pontos').value;
-    // this.currentMatch.handsOfMatch.push(hand)
+    hand.score = value;
+    this.currentMatch.handsOfMatch.push(hand)
   }
 
-  private initHand(player: Player) {
-    return this._fb.group({
-      pontos: ['0'],
-      estourou: [false],
-    });
-  }
 }
